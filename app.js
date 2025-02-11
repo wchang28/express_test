1f3c99b97b69d32ff06d506d4b74827c9f6e12c5
wchang28 <wchang28@hotmail.com>
2025-02-11 14:18:12 -0500

    updated

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 // can use the following env. vars. to control the web server
-// 1. HOSTNAME (optional)
+// 1. HOST (optional)
 // 1. PORT (optional)
 // 2. SSL_PRIVATE_KEY (optional)
 // 3. SSL_FULLCHAIN_CERT (optional)
 const http = require('http');
 const https = require('https');
-const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs')
-const path = require('path');
 
-let version = require('./package.json').version;
+const version = require('./package.json').version;
 
 console.log(`[${new Date().toISOString()}]: version=${version}`);
 
-let app = express();
-
-let bpj = bodyParser.json({"limit":"999mb"});   // json body middleware
-app.use(bpj);
+const app = express();
 
 app.use((req, res, next) => {
 	console.log('**********************************************************************');
@@ -27,21 +22,11 @@ app.use((req, res, next) => {
 	next();
 });
 
-//app.set("json replacer", null);
-//app.set("json spaces", 2);
-
 app.get('/', (req, res) => {
-	res.jsonp({now: new Date().toISOString(), msg: 'Hawdy', version});
+	res.jsonp({now: new Date().toISOString(), msg: 'Hello World!', version});
 });
 
-app.get('/exit', (req, res) => {
-	process.exit(1);
-});
-
-// for certbot "webroot" plugin certificate support
-app.use('/.well-known/acme-challenge', express.static(path.join(__dirname, '.well-known/acme-challenge')));
-
-let secure = (process.env.SSL_PRIVATE_KEY && process.env.SSL_FULLCHAIN_CERT ? true : false);
+const secure = (process.env.SSL_PRIVATE_KEY && process.env.SSL_FULLCHAIN_CERT ? true : false);
 
 let server = null;
 
@@ -55,13 +40,9 @@ if (secure) {
 	server = http.createServer(app);
 }
 
-let hostname = (process.env.HOSTNAME || "localhost");
-let port = (process.env.PORT || 8080);
-
-console.log(`[${new Date().toISOString()}]: hostname=${hostname}, port=${port}`);
+const hostname = (process.env.HOST || "0.0.0.0");
+const port = (process.env.PORT || 8080);
 
 server.listen(port, hostname, function () {
-	let host = server.address().address;
-	let port = server.address().port;
-	console.log(`[${new Date().toISOString()}]: app server listening at ${(secure ? 'https': 'http')}://${host}:${port}`);
+	console.log(`[${new Date().toISOString()}]: app server listening at ${(secure ? 'https': 'http')}://${hostname}:${port}`);
 });
